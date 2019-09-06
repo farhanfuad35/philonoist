3c3eb7fcabad53035bde2f0b9c1f35bbe7b767b3
farhanfuad35 <fuad_farhan@yahoo.com>
2019-09-06 23:08:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/philonoist/TuitionDetails.java
@@ -0,0 +1,36 @@
+package com.example.philonoist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class TuitionDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuition_details);
+            String[] subjects = new String[]{"Higher Math", "Physics", "Chemistry"};
+
+        ListView listView = findViewById(R.id.lvDetails_Subject);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, subjects);
+
+        listView.setAdapter(listViewAdapter);
+
+        TextView hostName = findViewById(R.id.tvDetails_hostName);
+        hostName.setText("Rahat Hossain");
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+}
