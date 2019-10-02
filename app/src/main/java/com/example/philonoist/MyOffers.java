8754f486138b23d6563dfee47bfc01c3d25ba34e
farhanfuad35 <fuad.24csedu.035@gmail.com>
2019-10-02 10:11:10 +0600

    Floating Action Button added for adding new Offer

--- app/src/main/java/com/example/philonoist/MyOffers.java
+++ app/src/main/java/com/example/philonoist/MyOffers.java
@@ -2,12 +2,15 @@ package com.example.philonoist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MyOffers extends AppCompatActivity {
 
@@ -16,13 +19,15 @@ public class MyOffers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_offers);
 
+        FloatingActionButton fab = findViewById(R.id.fab_add);
+
         setTitle("My Offers");
         androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar_MyOffer);
         setSupportActionBar(toolbar);
 
         String[] myOffers = new String[]{"Offer #1", "Offer #2", "Offer #3"};
 
-        ListView listView = findViewById(R.id.lvOffers_MyOffers);
+        final ListView listView = findViewById(R.id.lvOffers_MyOffers);
 
         ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myOffers);
 
@@ -32,6 +37,14 @@ public class MyOffers extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(listView.getContext(), com.example.philonoist.postOffer.class);
+                startActivity(intent);
             }
         });
     }
